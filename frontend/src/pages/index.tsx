57c57a84141f213fc3ce0f3c400abf8464fcc118
sharzil <sharzil.js@zeniuseducation.com>
2022-01-30 17:08:10 +0530

    Designed Homepage UI

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,16 @@
+import { NextPage } from "next";
+import { ProductGrid } from "src/features/product-grid";
+
+import { Container } from "@mui/material";
+
 import Layout from "@components/Layout";
 
-const IndexPage = () => <Layout>Home</Layout>;
+const IndexPage: NextPage = () => (
+  <Layout>
+    <Container sx={{ mt: 5 }}>
+      <ProductGrid />
+    </Container>
+  </Layout>
+);
 
 export default IndexPage;
--- frontend/src/shared/theme.ts
+++ frontend/src/shared/theme.ts
@@ -1,8 +1,23 @@
+import { red } from "@mui/material/colors";
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 let theme = createTheme({
   typography: {
     fontFamily: "Montserrat, sans-serif",
+  },
+  palette: {
+    primary: {
+      main: "#ff3e6c",
+    },
+    secondary: {
+      main: "#535766",
+    },
+    info: {
+      main: "#535766",
+    },
+    error: {
+      main: red.A400,
+    },
   },
 });
 
